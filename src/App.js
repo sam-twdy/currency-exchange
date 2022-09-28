bcc08ac97aaf13c153a9a365aacd91a7a1b667d6
Sam <stweedy.st@gmail.com>
2022-09-28 18:46:15 -0400

    currency graph

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import CurrencyExchange from './currencyExchange';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
 import Footer from './footer';
 import Navigation from './navbar';
+import CurrencyConverter from './currencyConverter';
 
 function App() {
   return (
-
+      
     <Router>
-
-      <Navigation />
-      <CurrencyExchange />
-      <Footer />
+        <Navigation />
+            <Switch>
+              <Route path="/" exact component={CurrencyExchange} />
+              <Route path="/currencyconverter" component={CurrencyConverter} />
+              <Route render={() => <h1>404 Not found</h1>} />
+            </Switch>
+        <Footer />
     </Router>
   );
 }
